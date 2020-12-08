59b0c6d5672c22bc51e2a8a904d70d6e05c44f7d
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-12-08 17:50:41 +0100

    ejercicio02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import array from "./array";
+import Print from "./printArray";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Print uno={array[0]} dos={array[1]} tres={array[2]} />;
 }
 
 export default App;
---
+++ src/printArray.js
@@ -0,0 +1,13 @@
+function Print (props){
+    return(
+        <>
+        <ul>
+            <li>{props.uno}</li>
+            <li>{props.dos}</li>
+            <li>{props.tres}</li>
+        </ul>
+        </>
+    )
+}
+
+export default Print;
